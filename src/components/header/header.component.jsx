88e4f3af6c65d6d13d694e05498e83e4be13fffb
elchin-imanov <eemanovcoop@gmail.com>
2020-12-30 10:07:07 +0400

    updated shop data  with new data, updated cart utils to group items in cartReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -29,9 +29,9 @@ const Header = ({ currentUser, hidden }) => (
             <CartIcon />
         </div>
         {
-            hidden ?
-            <CartDropdown />
-            : null
+            hidden ? (
+              <CartDropdown />
+            ) : null
         }
     </div>
 );
